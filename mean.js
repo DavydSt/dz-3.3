c18d51fb5f0d481785eb1c56410994fc502a98c6
DavydSt <145857522+DavydSt@users.noreply.github.com>
2025-01-27 18:17:36 +0100

    finish

---
+++ mean.js
@@ -0,0 +1,26 @@
+// 1 =======
+
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+// 2 =======
+
+const email = "davyd.str@outlook.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+// 3 =======
+
+const my = "My";
+const name = " name ";
+const is = "is";
+const fullName = my + name + is + " Viktor";
+console.log(fullName);
+
+// 4 =======
+
+
+const userName = "Стас";
+const payment = "228 грн";
+alert("Дякуємо, " + userName + " ! До сплати " + payment )
